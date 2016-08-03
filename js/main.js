c3070cb50063467c8bd223d9272e09bcbab3ae31
qwazik <qwazik21@gmail.com>
2016-08-03 15:48:36 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,25 +1,81 @@
 //myPlugins
   ;(function($){
     $.fn.qTabs = function(){
-        var global = this;
-        global.find('.tabs-content__item').hide();
-        global.find('.tabs-content__item.active').show();
+        var main = this;
+        main.find('.tabs-content__item').hide();
+        main.find('.tabs-content__item.active').show();
         $(this).find('.tabs-nav li').click(function(){
             $(this).addClass('active');
             $(this).siblings().removeClass('active');
             var data = $(this).find('a').attr('href');
-            $(global).find('.tabs-content__item').hide().removeClass('active');
-            $(global).find('.tabs-content__item' + data + '').fadeIn(300).addClass('active');
+            $(main).find('.tabs-content__item').hide().removeClass('active');
+            $(main).find('.tabs-content__item' + data + '').fadeIn(300).addClass('active');
             return false;
         })
     }
 
     $.fn.qToggle = function(){
-        var global = this;
+        var main = this;
         $(this).click(function(e){
             e.preventDefault();
             $(this).addClass('active');
             $(this).siblings().removeClass('active');
         })
     }
+    $.fn.placeholder = function(){
+        var main = this;
+    }
   }(jQuery));
+
+  $(document).ready(function(){
+
+    $('#mainCarousel').owlCarousel({
+        infinite:true,
+        loop:true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        aoutplayHoverPause: true,
+        items: 1,
+        dots: true,
+    });
+    $('.main-carousel .owl-controls').css({
+        'margin-left': - ($('.main-carousel .owl-controls').width() / 2) + 'px'
+    })
+
+    //выпадающее меню
+    $('.dropdown').hover(function () {
+         clearTimeout($.data(this,'timer'));
+         $('ul',this).stop(true,true).fadeIn(200);
+      }, function () {
+        $.data(this,'timer', setTimeout($.proxy(function() {
+          $('ul',this).stop(true,true).fadeOut(200);
+        }, this), 100));
+      });
+
+    //fancybox init
+    $('.fancybox').fancybox({
+        padding: 0,
+        wrapCSS: 'fancy-callback'
+    });
+
+    //filter select
+    $('.catalog-filter select').selectmenu();
+    //initiate the plugin and pass the id of the div containing gallery images
+    $("#img_01").elevateZoom({
+        scrollZoom : true,
+        gallery:'gal1',
+        cursor: 'pointer', 
+        galleryActiveClass: 'active', 
+        imageCrossfade: true, 
+        loadingIcon: 'img/giphy.gif'
+    }); 
+
+    //pass the images to Fancybox
+    $("#img_01").bind("click", function(e) {  
+      var ez =   $('#img_01').data('elevateZoom'); 
+        $.fancybox(ez.getGalleryList());
+      return false;
+    });
+  })
+
+ 
